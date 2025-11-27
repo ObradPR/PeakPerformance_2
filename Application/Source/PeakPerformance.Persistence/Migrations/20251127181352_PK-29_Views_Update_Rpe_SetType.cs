cfd3f273465981cee9ec10ef6b11a944bf2c40ef
ObradPR <obradpualic@gmail.com>
2025-11-27 19:15:03 +0100

    PK-29 - Workouts page - MIGRATION - Additional views

---
+++ Application/Source/PeakPerformance.Persistence/Migrations/20251127181352_PK-29_Views_Update_Rpe_SetType.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+
+namespace PeakPerformance.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PK29_Views_Update_Rpe_SetType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Up<eSetRpeType>();
+            migrationBuilder.Up<eSetType>();
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
