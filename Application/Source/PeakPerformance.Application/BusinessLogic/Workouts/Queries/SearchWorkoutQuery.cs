3cd8ab448393f8498f88970d0e66e99444fd4b1b
ObradPR <obradpualic@gmail.com>
2025-11-30 23:37:55 +0100

    PK-29 - Workouts page - Back-end - fix total pages for pagination

--- Application/Source/PeakPerformance.Application/BusinessLogic/Workouts/Queries/SearchWorkoutQuery.cs
+++ Application/Source/PeakPerformance.Application/BusinessLogic/Workouts/Queries/SearchWorkoutQuery.cs
@@ -94,7 +94,7 @@ public class SearchWorkoutQuery(WorkoutSearchOptions options) : IRequest<Respons
             return new(new()
             {
                 Data = data,
-                Total = result.Total - 1
+                Total = result.Total
             });
         }
     }
