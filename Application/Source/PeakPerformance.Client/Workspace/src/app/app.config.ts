d46a9713231b6d9f547b2fbc23a2c04342cff4fd
ObradPR <obradpualic@gmail.com>
2025-12-02 17:03:17 +0100

    PK-29 - Workouts page - Front-end - Navigating to new page , should set you on top of that page

--- Application/Source/PeakPerformance.Client/Workspace/src/app/app.config.ts
+++ Application/Source/PeakPerformance.Client/Workspace/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, Provider, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -18,7 +18,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'top'
+    })),
     provideAnimations(),
     provideAnimationsAsync(),
     provideHttpClient(
