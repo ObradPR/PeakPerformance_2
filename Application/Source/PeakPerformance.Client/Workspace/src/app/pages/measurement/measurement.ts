b44b05d605bf73afaf1f65cd98d5faa52366344d
ObradPR <obradpualic@gmail.com>
2025-12-07 12:52:05 +0100

    PK-20 - Measurement page - Front-end - changing the legend style to match the chartjs

--- Application/Source/PeakPerformance.Client/Workspace/src/app/pages/measurement/measurement.ts
+++ Application/Source/PeakPerformance.Client/Workspace/src/app/pages/measurement/measurement.ts
@@ -533,6 +533,16 @@ export class Measurement implements OnDestroy {
     })
   }
 
+  getTransparentColor(hex: string): string {
+    if (!hex.startsWith('#')) return hex;
+
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+
+    return `rgba(${r}, ${g}, ${b}, 0.2)`;
+  }
+
   ngOnDestroy(): void {
     this.destroyChart();
   }
